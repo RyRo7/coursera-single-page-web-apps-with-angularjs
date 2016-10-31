f3c8df7b32658344afc41585a9ae292ed4c84bde
RyRo <ryan.roberts@jumpco.io>
2016-10-31 07:44:29 +0200

    added colourings

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -14,13 +14,16 @@ function LunchCheckController($scope) {
     //replace removes any possible white space between separators, as well as removing any 'null' items in the lunch list
     //the flag g=global match; find all matches rather than stopping after the first match
     //split iterates through lunch list and creates the array of lunch items
-    var lunchList = $scope.lunchMenu.replace(findThis, ',').split(',');
-    console.log(lunchList);
-    if (lunchList == null) {
-      $scope.checkLunchMessage = "Please enter data first!";
+
+    if ($scope.lunchMenu == "") {
+      $scope.checkLunchMessage = "Please enter data first";
+      $scope.colour = "color:red";
     } else {
+      var lunchList = $scope.lunchMenu.replace(findThis, ',').split(',');
+      console.log(lunchList);
       $scope.checkLunchMessage = checkLunchSize(lunchList.length);
     }
+
   }
 
   function checkLunchSize(size) {
@@ -28,18 +31,15 @@ function LunchCheckController($scope) {
     var response = "";
 
     switch (size) {
-      case 0:
-        response = "Please enter data first";
-        break;
       case 1:
       case 2:
-        response = "Still good. Keep adding to the food pile!";
-        break;
       case 3:
-        response = "At the limit for what your plate can hold. Best not to add any more";
+        response = "Enjoy!";
+        $scope.colour = "color:green";
         break;
       default:
-        response = "Woah there tubby! Got quite a plate full there - put something back!";
+        response = "Too Much!";
+        $scope.colour = "color:red";
         break;
     }
 
