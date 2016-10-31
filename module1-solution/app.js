2d522fee73aee7c9c84b6990772865e7ccedf925
RyRo <ryan.roberts@jumpco.io>
2016-10-31 07:49:54 +0200

    reorganised the comments

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -9,27 +9,22 @@ function LunchCheckController($scope) {
   $scope.checkLunchMessage = "";
 
   $scope.CheckLunch = function() {
-    console.log($scope.lunchMenu);
-    var findThis = /,\s*,/g;
-    //replace removes any possible white space between separators, as well as removing any 'null' items in the lunch list
-    //the flag g=global match; find all matches rather than stopping after the first match
-    //split iterates through lunch list and creates the array of lunch items
-
     if ($scope.lunchMenu == "") {
       $scope.checkLunchMessage = "Please enter data first";
       $scope.colour = "color:red";
     } else {
+      var findThis = /,\s*,/g;
+      //the flag g=global match; find all matches rather than stopping after the first match
+
+      //replace removes any possible white space between separators, as well as removing any 'null' items in the lunch list
+      //split iterates through lunch list and creates the array of lunch items
       var lunchList = $scope.lunchMenu.replace(findThis, ',').split(',');
-      console.log(lunchList);
       $scope.checkLunchMessage = checkLunchSize(lunchList.length);
     }
-
   }
 
   function checkLunchSize(size) {
-    //console.log(size);
     var response = "";
-
     switch (size) {
       case 1:
       case 2:
@@ -42,7 +37,6 @@ function LunchCheckController($scope) {
         $scope.colour = "color:red";
         break;
     }
-
     return response;
   }
 }
