73e69eff9dd87c29b7490dfa31d2221538b3d004
RyRo <ryan.roberts@jumpco.io>
2016-11-03 12:01:57 +0200

    cleaned up solution for assignment submission

--- module2-solution/app.js
+++ module2-solution/app.js
@@ -1,5 +1,49 @@
-angular.module('ShoppingListCheckOff', [
-  'ToBuyControllerMod',
-  'AlreadyBoughtControllerMod',
-  'ShoppingListCheckOffServiceMod',
-]);
+(function () {
+'use strict';
+
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService) {
+  var showToBuyList = this;
+  showToBuyList.items = ShoppingListCheckOffService.toBuyItems;
+
+  showToBuyList.buyItem = function (itemIndex) {
+    ShoppingListCheckOffService.buyItem(itemIndex);
+  };
+}
+
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService) {
+  var showBoughtList = this;
+  showBoughtList.items = ShoppingListCheckOffService.boughtItems;
+}
+
+function ShoppingListCheckOffService() {
+  var service = this;
+  // List of shopping items
+  var toBuyItems = [
+    { name: "Milk", quantity: "2" },
+    { name: "Donuts", quantity: "200" },
+    { name: "Cookies", quantity: "300" },
+    { name: "Chocolate", quantity: "5" },
+    { name: "Peanut Butter", quantity: "2" }
+  ];
+  var boughtItems = [];
+
+  service.buyItem = function (itemIndex) {
+    var item = {
+      name: toBuyItems[itemIndex].name,
+      quantity: toBuyItems[itemIndex].quantity
+    };
+    toBuyItems.splice(itemIndex, 1);
+    boughtItems.push(item);
+  };
+
+  service.toBuyItems = toBuyItems;
+  service.boughtItems = boughtItems;
+}
+})();
