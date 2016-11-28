5fd95f7292637607837c6d1dbe18b2202f4d7c3c
RyRo <ryan.roberts@jumpco.io>
2016-11-28 15:06:13 +0200

    updated labels

--- module5-solution/src/public/signup/signup.controller.js
+++ module5-solution/src/public/signup/signup.controller.js
@@ -8,12 +8,14 @@ SignUpController.$inject = ['UserInfoService', 'MenuService'];
 function SignUpController(UserInfoService, MenuService) {
   var $ctrl = this;
   $ctrl.userdata = {};
+  $ctrl.validDish = false;
 
   $ctrl.submit = function () {
     MenuService.getMenuItem($ctrl.userdata.dish)
      .then(
        function (response) {
           $ctrl.userdata.favouriteDish = response;
+          $ctrl.validDish = true;
           UserInfoService.addUserToPsuedoDatabase($ctrl.userdata);
         }
       );
